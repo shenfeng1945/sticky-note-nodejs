2891c334b3f864dec26862f049a77f2895d6c41e
shenfeng1945 <823902826@qq.com>
2018-03-09 18:10:01 +0800

    remove

--- app.js
+++ app.js
@@ -16,12 +16,14 @@ app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+//中间件
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+//路由
 app.use('/', index);
 app.use('/users', users);
 
