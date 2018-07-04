03f1a9e50050a7b5de1494e0217d5bc93ec5602d
shenfeng1945 <823902826@qq.com>
2018-07-04 20:00:14 +0800

    note.js

--- src/js/mod/note.js
+++ src/js/mod/note.js
@@ -1,22 +1,49 @@
 var Toast = require('./toast.js').Toast
+var Event = require('./event.js').Event
 
-function Note(opts){
-    
+function Note(opts) {
+
 }
 Note.prototype = {
-   color:[],
-   defaultOpts:{
-       id:'',
-       $ct:$('#content').length>0?$('#content'):$('body'),//容器
-       context:'input here'//内容
-   }, 
-   initOpts:function(opts){
-     this.opts = $.extend({},this.defaultOpts,opts||{})
-     if(this.opts.id){
-         this.id = this.opts.id;
-     }
-   },
-   createNote:function(){
-       var tpl = ``
-   }
+    defaultOpts: {
+        id: '',
+        $ct: $('#content').length > 0 ? $('#content') : $('body'), //容器
+        context: 'input here' //内容
+    },
+    initOpts: function (opts) {
+        this.opts = $.extend({}, this.defaultOpts, opts || {})
+        if (this.opts.id) {
+            this.id = this.opts.id;
+        }
+    },
+    createNote: function () {
+        var tpl = ` <div class="note">
+                      <div class="head-note">
+                         <div class="time"></div>
+                         <div class="close">
+                            <svg class="icon icon-20">
+                                <use xlink:href="#icon-close"></use>
+                            </svg>
+                          </div>
+                       </div>
+                       <div class="context">
+                       </div>
+                    </div>`
+        this.$note = $(tpl)
+        this.$note.find('.context').text(this.opts.context);
+        this.opts.$ct.append(this.$note);
+    },
+    setLayout:function(){
+        var self = this;
+        if(self.clk){
+            clearTimeout(self.clk)
+        }
+        self.clk = setTimeout(function(){
+            Event.fire('waterfall')
+        },100)
+    },
+    bindEvent:function(){
+        
+    }
+    
 }
